a319addb56217c8fb62c2779a712d9d6cd3ea49a
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-03-19 02:09:33 +0300

    Router connection with server is made

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"log"
 	"github.com/gin-gonic/gin"
+	"github.com/aabdullahgungor/mybookcase/router"
 )
 
 type Server struct {
@@ -18,4 +19,8 @@ func NewServer() Server {
 }
 
 func (s *Server) Run() {
+	router := routes.ConfigRoutes(s.server)
+
+	log.Printf("Server running at port: %v", s.port)
+	log.Fatal(router.Run(":" + s.port))
 }
