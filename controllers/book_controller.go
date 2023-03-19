5a5ab9056385daa21239cb06cba437e90a615bc3
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-03-19 22:59:37 +0300

    datatransfer file created

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -2,18 +2,18 @@ package controllers
 
 import (
 	//"strconv"
-	"net/http"
+	//"net/http"
 	"github.com/gin-gonic/gin"
-	"github.com/aabdullahgungor/mybookcase/database"
-	"github.com/aabdullahgungor/mybookcase/models"
+	//"github.com/aabdullahgungor/mybookcase/database"
+	//"github.com/aabdullahgungor/mybookcase/models"
 )
 
 func GetAllBooks(c *gin.Context)  {
 
-	db, _:= database.GetDB()
-	var books []models.Book
-	db.Find(&books)
-	c.IndentedJSON(http.StatusOK, books)
+	// db, _:= database.GetDB()
+	// var books []models.Book
+	// db.Find(&books)
+	// c.IndentedJSON(http.StatusOK, books)
 }
 
 func GetBook(c *gin.Context)  {
