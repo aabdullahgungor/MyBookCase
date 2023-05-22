dff7fc3e667adabe4e560b4e6e972fe03c239dec
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-05-22 16:35:45 +0300

    reader entity updated

--- entities/reader_entity.go
+++ entities/reader_entity.go
@@ -3,9 +3,11 @@ package entities
 import "fmt"
 
 type Reader struct {
-	ID      uint `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
-	Name    string `gorm:"column:name" json:"name"`
-	Books []Book `gorm:"ForeignKey:ReaderID"`
+	ID       uint   `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
+	Name     string `gorm:"column:name" json:"name"`
+	Email    string `gorm:"column:email" json:"email"`
+	Password string `gorm:"column:password" json:"password"`
+	Books    []Book `gorm:"ForeignKey:ReaderID"`
 }
 
 func (reader *Reader) TableName() string {
