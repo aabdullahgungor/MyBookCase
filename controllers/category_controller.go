403623ba7d80c4cd0510250c4f10d239d552171d
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-06-08 22:04:15 +0300

    category controller updated for Swagger

--- controllers/category_controller.go
+++ controllers/category_controller.go
@@ -9,18 +9,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
-type CategoryController struct{
+type CategoryController struct {
 }
 
-func (C CategoryController) GetAll(c *gin.Context)  {
+// GetCategories           godoc
+// @Summary		Get categories array
+// @Description	Responds with the list of all categories as JSON.
+// @Tags			categories
+// @Produce		json
+// @Success		200	{object}	entities.Category
+// @Router			/categories [get]
+func (C CategoryController) GetAll(c *gin.Context) {
 	var categoryModel models.CategoryModel
 	categories, _ := categoryModel.GetAll()
 	c.Header("Content-Type", "application/json")
-	c.IndentedJSON(http.StatusOK, categories)	
+	c.IndentedJSON(http.StatusOK, categories)
 }
 
-func (C CategoryController) GetById(c *gin.Context)  {
+// GetCategory           godoc
+// @Summary		Get single category by id
+// @Description	Returns the category whose id value matches the id.
+// @Tags			categories
+// @Produce		json
+// @Param			id path	string true "search category by id"
+// @Success		200		{object}	entities.Category
+// @Failure 	400     error message
+// @Failure 	406     error message
+// @Router			/categories/{id} [get]
+func (C CategoryController) GetById(c *gin.Context) {
 	str_id := c.Param("id")
 	int_id, errId := strconv.Atoi(str_id)
 	if errId != nil {
@@ -29,7 +45,7 @@ func (C CategoryController) GetById(c *gin.Context)  {
 		})
 		return
 	}
-	
+
 	var categoryModel models.CategoryModel
 	category, err := categoryModel.GetById(int_id)
 	if err != nil {
@@ -41,7 +57,17 @@ func (C CategoryController) GetById(c *gin.Context)  {
 	c.IndentedJSON(http.StatusOK, category)
 }
 
-func (C CategoryController) Create(c *gin.Context)  {
+// CreateCategory           godoc
+// @Summary		Add a new category
+// @Description	Takes a category JSON and store in DB. Return saved JSON.
+// @Tags			categories
+// @Produce		json
+// @Param			category body	entities.Category	true "Category JSON"
+// @Success		200		{object}	entities.Category
+// @Failure 	400     error message
+// @Failure 	406     error message
+// @Router			/categories [post]
+func (C CategoryController) Create(c *gin.Context) {
 	var category entities.Category
 	err := c.ShouldBindJSON(&category)
 	if err != nil {
@@ -60,11 +86,21 @@ func (C CategoryController) Create(c *gin.Context)  {
 		return
 	}
 
-	c.IndentedJSON(http.StatusCreated, gin.H{"message":"Category has been created","category_id": category.ID})
-	
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "Category has been created", "category_id": category.ID})
+
 }
 
-func (C CategoryController) Edit(c *gin.Context)  {
+// EditCategory          godoc
+// @Summary		Edit an category
+// @Description	Takes a category JSON and edit an in DB. Return saved JSON.
+// @Tags			categories
+// @Produce		json
+// @Param			category body	entities.Category	true "Category JSON"
+// @Success		200		{object}	entities.Category
+// @Failure 	400     error message
+// @Failure 	406     error message
+// @Router			/categories [put]
+func (C CategoryController) Edit(c *gin.Context) {
 	var category entities.Category
 	err := c.ShouldBindJSON(&category)
 	if err != nil {
@@ -83,12 +119,22 @@ func (C CategoryController) Edit(c *gin.Context)  {
 		return
 	}
 
-	c.IndentedJSON(http.StatusAccepted, gin.H{"message":"Category has been edited","category_id": category.ID})
-	
+	c.IndentedJSON(http.StatusAccepted, gin.H{"message": "Category has been edited", "category_id": category.ID})
+
 }
 
-func (C CategoryController) Delete(c *gin.Context)  {
-	
+// DeleteCategory          godoc
+// @Summary		Delete an category
+// @Description	Remove an category from DB by id.
+// @Tags			categories
+// @Produce		json
+// @Param			id path	string true "delete category by id"
+// @Success		200		{object}	entities.Category
+// @Failure 	400     error message
+// @Failure 	406     error message
+// @Router			/categories/{id} [delete]
+func (C CategoryController) Delete(c *gin.Context) {
+
 	str_id := c.Param("id")
 	int_id, errId := strconv.Atoi(str_id)
 
@@ -110,5 +156,5 @@ func (C CategoryController) Delete(c *gin.Context)  {
 		return
 	}
 
-	c.IndentedJSON(http.StatusAccepted, gin.H{"message":"Category has been deleted","category_id": int_id})
+	c.IndentedJSON(http.StatusAccepted, gin.H{"message": "Category has been deleted", "category_id": int_id})
 }
